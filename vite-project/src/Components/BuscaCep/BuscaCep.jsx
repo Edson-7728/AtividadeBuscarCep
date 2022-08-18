738bb6e9599800813c7edc6241700fd34a2195a5
PatrickCarrati <patrickferrari.nft@outlook.com>
2022-08-18 20:32:42 -0300

    completando o componente buscaCep

--- vite-project/src/Components/BuscaCep/BuscaCep.jsx
+++ vite-project/src/Components/BuscaCep/BuscaCep.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import {useState} from 'react'
+import Fetch from '../../Fetch/Fetch'
+import Button from '../Button/Button'
+import Card from '../Card/Card'
 
 const BuscaCep = () => {
-    const [cep, setCep] = useState()
-    async function handleRequisicao() {
-        const response = await fetch ('https://viacep.com.br/ws/01001000/json/')
+    const [cep, setCep] = useState("")
+     function handleRequisicao() {
+       setDados(Fetch(cep)) 
     }
+    const [dados, setDados] = useState([])
+     
   return (
 
     <div>
-
+    <input type= "text" placeholder='cep' onChange={(e)=> {setCep (e.target.value)}} value={cep}></input>
+    <Button onClick={handleRequisicao} texto ="Busca Cep" />
+    {dados.map((dados,index)=>{
+      return <Card key={index} cep={dados.cep} logradouro={dados.logradouro} complemento={dados.complemento} bairro={dados.bairro} localidade= {dados.localidade} uf={dados.uf} />
+    })}
     </div>
   )
 }
