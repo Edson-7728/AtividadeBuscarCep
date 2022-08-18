ca79a4e6590e3bf6eb7747caadc5e800668ca3e5
PatrickCarrati <patrickferrari.nft@outlook.com>
2022-08-18 20:33:02 -0300

    Completando o componente Card

--- vite-project/src/Components/Card/Card.jsx
+++ vite-project/src/Components/Card/Card.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const Card = () => {
-  return (
-    
+
+
+const Card = ({cep,logradouro,complemento,bairro,localidade,uf}) => {
+  return (    
     <div>
-        
+       <h4>{cep}</h4>
+       <br></br><h4> {logradouro} </h4> 
+       <br></br><h4>{complemento}</h4>
+       <br></br><h4>{bairro}</h4>
+       <br></br><h4>{localidade}</h4>
+       <br></br><h4>{uf}</h4> 
     </div>
   )
 }
